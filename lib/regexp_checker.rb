96bc62cf3e192eec0d39dc84f73d5e0f0444523a
Ryunosuke Abe <ryu212920@gmail.com>
2020-08-21 12:55:19 +0900

    finish 9

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -1,4 +1,19 @@
 print 'Text?:'
 text = gets.chomp
-print 'Pattern?:'
-pattern = gets.chomp
+
+begin
+  print 'Pattern?: '
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError => e
+  puts "Invalid pattern: #{e.message}"
+  retry
+end
+
+regexp = Regexp.new(pattern)
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matches: #{matches.join(', ')} "
+else
+  puts "Nothing matched."
+end
